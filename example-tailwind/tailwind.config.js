b9b01c5c383357c5baf40ba3acee15551ee6247f
Carsten Munk <carsten.munk@zippie.org>
2020-10-14 21:40:55 +0200

    Add tailwind example
    Signed-off-by: Carsten Munk <carsten.munk@zippie.org>

---
+++ example-tailwind/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [ 'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html'],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: [
+    ({ addComponents }) => {
+      const AppLogo = {
+        '.App-logo': {
+          animation: 'App-logo-spin infinite 20s linear',
+          height: '40vmin',
+          'pointer-events': 'none'
+        },
+        '@keyframes App-logo-spin': {
+          from: {
+            transform: 'rotate(0deg)'
+          },
+          to: {
+            transform: 'rotate(360deg)'
+          }
+        }
+      }
+      addComponents(AppLogo)
+    }
+  ]
+}
