8de3c0a9e7c08b785d478390870468eebb5860ad
MioQuispe <mioq91@gmail.com>
2020-10-26 21:17:01 +0100

    Create snowpack project & replace its default dev server

---
+++ mio-playground/es-dev-server.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    port: 8080,
+    watch: true,
+    nodeResolve: true,
+    appIndex: 'build/index.html',
+    rootDir: 'build/',
+    cors: true,
+    plugins: [],
+    moduleDirs: ['node_modules', 'web_modules'],
+};
